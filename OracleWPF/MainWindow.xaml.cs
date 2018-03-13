0ca95c2b367fd020cf8448af988d43e8f429bc0a
Roberto de Lima <robertodelimasouza@gmail.com>
2018-03-13 19:31:44 -0300

    Add project files.

---
+++ OracleWPF/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
+using System.Data;
+
+namespace OracleWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string connString = "";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connString = BuildConnString();
+        }
+
+
+
+        #region Database
+
+        private void LoadDatabase(string Filter)
+        {
+            using (var conn = new OracleConnection(connString))
+            {
+                dataGridOracle.DataContext = null;
+
+                conn.Open();
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from pedido";
+
+                if (Filter != "")
+                {
+                    cmd.CommandText += $" WHERE LOWER(CLIENTE) LIKE LOWER('%{Filter}%')";
+                }
+
+                cmd.CommandType = CommandType.Text;
+                
+
+                DataSet ds = new DataSet();
+                OracleDataAdapter da = new OracleDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(ds);
+                dataGridOracle.DataContext = ds.Tables[0];
+
+            }
+        }
+
+        private string BuildConnString()
+        {
+            return "User Id=beto; password=betopass; Data Source=127.0.0.1:1521/XE;";
+        }
+
+        public bool RunSQL(string SQL, bool showMessageBox = false)
+        {
+            try
+            {
+                using (var conn = new OracleConnection(connString))
+                {
+                    conn.Open();
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.Connection = conn;
+
+                    cmd.CommandText = SQL;
+
+                    cmd.ExecuteScalar();
+
+                    return true;
+                
+                }
+            }
+            catch (Exception ex)
+            {
+                if (showMessageBox) MessageBox.Show("Erro ao executar comando! - " + ex.ToString());
+
+                return false;
+            }
+            
+        }
+
+        private void DeletePedido(string ID)
+        {
+
+            var result = MessageBox.Show("Confirma exclusão do pedido?", "EXCLUSÃO", MessageBoxButton.YesNo, MessageBoxImage.Exclamation, MessageBoxResult.No);
+            if (result == MessageBoxResult.No) return;
+
+            RunSQL("DELETE FROM PEDIDO WHERE ID = " + ID);
+
+            LoadDatabase(textBoxFiltro.Text);
+        }
+
+        #endregion
+
+        #region Control Events
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadDatabase("");
+        }
+
+
+        private void UpdatePedido()
+        {
+            var ID = dataGridOracle.SelectedValue.ToString();
+            var pedWin = new PedidoWindow(ID);
+            pedWin.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            pedWin.Closed += PedWin_Closed;
+            pedWin.ShowDialog();
+        }
+
+        private void dataGridOracle_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dataGridOracle.SelectedValue == null) return;
+
+            UpdatePedido();
+        }
+
+
+        private void buttonFiltro_Click(object sender, RoutedEventArgs e)
+        {
+            LoadDatabase(textBoxFiltro.Text);
+        }
+
+        private void buttonNovo_Click(object sender, RoutedEventArgs e)
+        {
+            var pedWin = new PedidoWindow("");
+            pedWin.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            pedWin.Closed += PedWin_Closed;
+            pedWin.ShowDialog();
+        }
+
+        private void PedWin_Closed(object sender, EventArgs e)
+        {
+            LoadDatabase(textBoxFiltro.Text);
+        }
+
+        #endregion
+
+        private void dataGridOracle_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                if (dataGridOracle.SelectedValue != null)
+                {
+                    DeletePedido(dataGridOracle.SelectedValue.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ OracleWPF/PedidoWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OracleWPF
+{
+    /// <summary>
+    /// Interaction logic for PedidoWindow.xaml
+    /// </summary>
+    public partial class PedidoWindow : Window
+    {
+        string ID = "";
+        MainWindow main;
+        public PedidoWindow(string id)
+        {
+            InitializeComponent();
+
+            main = ((MainWindow)App.Current.MainWindow);
+            ID = id;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ID != "")
+            {
+                LoadPedido();
+            }
+            else
+            {
+                GetNextID();
+            }
+        }
+
+
+        private void GetNextID()
+        {
+            using (var conn = new OracleConnection(main.connString))
+            {
+                OracleCommand cmd = new OracleCommand($"SELECT MAX(ID) as NextID from PEDIDO", conn);
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                var dr = cmd.ExecuteReader();
+                dr.Read();
+
+                var id = dr.GetOracleDecimal(dr.GetOrdinal("NEXTID"));
+                IDTextBox.Text = (id + 1).ToString();
+            }
+        }
+
+        private void LoadPedido()
+        {
+            using (var conn = new OracleConnection(main.connString))
+            {
+                OracleCommand cmd = new OracleCommand($"SELECT * from PEDIDO where ID = {ID}", conn);
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                var dr = cmd.ExecuteReader();
+
+
+                dr.Read();
+
+                var id = ID;
+                var Cliente = dr.GetOracleString(dr.GetOrdinal("CLIENTE"));
+                var Data = dr.GetOracleDate(dr.GetOrdinal("DATA"));
+                var total = dr.GetOracleDecimal(dr.GetOrdinal("TOTAL"));
+
+                IDTextBox.Text = ID;
+                CLIENTETextBox.Text = Cliente.Value;
+                DATADatePicker.SelectedDate = Data.Value;
+                TOTALTextBox.Text = string.Format("{0:N}", total);
+
+
+            }
+        }
+
+
+        private bool Validar()
+        {
+            var erros = "";
+
+            if (CLIENTETextBox.Text == "") erros += "Nome do Cliente Vazio!\n";
+            if (TOTALTextBox.Text == "") erros += "Total Vazio!\n";
+            else
+            {
+                decimal total = 0;
+                if (!decimal.TryParse(TOTALTextBox.Text, out total)) erros += "Total não é um número válido!\n";
+            }
+            if (DATADatePicker.SelectedDate == null) erros += "Selecione uma data!\n";
+
+
+            if (erros.Length == 0) return true;
+            else
+            {
+                MessageBox.Show(erros);
+                return false;
+            }
+        }
+
+
+        private void SalvarPedido()
+        {
+
+            if (!Validar()) return;
+
+            var id = IDTextBox.Text;
+            var cliente = CLIENTETextBox.Text;
+            var data = DATADatePicker.SelectedDate.Value.ToShortDateString();
+            var total = TOTALTextBox.Text.Replace(".", "").Replace(",", ".");
+
+            if (ID == "")
+            {
+                var ins = $"INSERT INTO PEDIDO (ID, CLIENTE, DATA, TOTAL) VALUES ({id}, '{cliente}', '{data}', {total})";
+                main.RunSQL(ins);
+            }
+            else
+            {
+                var upd = $"UPDATE PEDIDO SET CLIENTE = '{cliente}', DATA = '{data}', TOTAL = {total} WHERE ID = {id} ";
+                main.RunSQL(upd);
+            }
+
+            MessageBox.Show("Pedido Salvo!");
+            Close();
+        }
+
+        private void SalvarButton_Click(object sender, RoutedEventArgs e)
+        {
+            SalvarPedido();
+          
+        }
+    }
+}
